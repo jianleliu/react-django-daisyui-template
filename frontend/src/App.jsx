78f316a4760ea19a57cb6d53cdfd7d4a92c856fe
Jianle <liujl@hawaii.edu>
2024-09-03 21:50:39 -1000

    * moved daisyui and tailwindcss to devdependency. * Confirmed daisyui working.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MyApp from './components/MyApp';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <MyApp />
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">DaisyUI Card</h2>
+            <p>This is a test card using DaisyUI styles.</p>
+          </div>
+        </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-],
+    './**/*.{jsx, js, ts, tsx}',
+  ],
   theme: {
     extend: {},
   },
